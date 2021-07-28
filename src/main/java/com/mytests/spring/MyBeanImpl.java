025d5b7cb4466780eded281f94f95d0687df2df7
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-07-28 18:12:04 +0300

    comment

--- src/main/java/com/mytests/spring/MyBeanImpl.java
+++ src/main/java/com/mytests/spring/MyBeanImpl.java
@@ -1,7 +1,7 @@
 package com.mytests.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+ // https://youtrack.jetbrains.com/issue/IDEA-150717
 public class MyBeanImpl implements MyBean {
     
     @Autowired
